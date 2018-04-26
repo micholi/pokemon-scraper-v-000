1b6b4858f72b2cb48d95eff29e333fdf5e709524
Michelle Olivieri <michelleo72@gmail.com>
2018-04-26 18:24:04 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,10 @@ class Pokemon
 
   attr_accessor :name, :type, :db, :id
 
-  def initialize(name, type, db)
+  def initialize(name)
     @name = name
-    @type = type
-    @db = db
+    #@type = type
+    #@db = db
   end
 
   def self.save(name, type, id)
